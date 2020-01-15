3b363a25a4737f0be0d6b96870fcaf3311bd96aa
Hoàng Hải Hà <hhh@netcompany.com>
2020-01-15 19:31:36 +0700

    nth commit

--- middlewareCustom/simpleLogger.go
+++ middlewareCustom/simpleLogger.go
@@ -1,20 +1,37 @@
 package middlewareCustom
 
 import (
+	"io"
 	"log"
-
-	"github.com/labstack/echo/v4"
+	"os"
 )
 
-func SimpleLogger() echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			req := c.Request()
-			err := next(c)
-			res := c.Response()
-			log.Println(req.URL.Host, req.URL.Path, req.Method, res.Status)
-			return err
-		}
+var Logger Customlogger
 
-	}
+type Customlogger struct {
 }
+
+func (p Customlogger) Write(data []byte) (n int, err error) {
+	f, err := os.OpenFile("D:/GoNordic/go2/nc_student/logger/Http.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+	wrt := io.MultiWriter(os.Stdout, f)
+	log.SetOutput(wrt)
+	log.Println(string(data))
+	return len(data), nil
+}
+
+// func SimpleLogger() echo.MiddlewareFunc {
+// 	return func(next echo.HandlerFunc) echo.HandlerFunc {
+// 		return func(c echo.Context) error {
+// 			req := c.Request()
+// 			err := next(c)
+// 			res := c.Response()
+// 			log.Println(req.URL.Host, req.URL.Path, req.Method, res.Status)
+// 			return err
+// 		}
+
+// 	}
+// }
